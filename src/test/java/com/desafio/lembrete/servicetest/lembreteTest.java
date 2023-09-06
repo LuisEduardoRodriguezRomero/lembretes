b42bd1b82cc7277f29f016db7ef4d98061b6259a
Luis Rodriguez <luiserr1394@gmail.com>
2023-09-05 21:32:53 -0300

    Testes 2

--- src/test/java/com/desafio/lembrete/servicetest/lembreteTest.java
+++ src/test/java/com/desafio/lembrete/servicetest/lembreteTest.java
@@ -1,10 +1,19 @@
 package com.desafio.lembrete.servicetest;
 
 
+import com.desafio.lembrete.entity.Lembrete;
+import com.desafio.lembrete.entity.Pessoa;
+import com.desafio.lembrete.repository.PessoaRepository;
 import com.desafio.lembrete.service.LembreteService;
 import com.desafio.lembrete.service.PessoaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class lembreteTest {
@@ -13,12 +22,28 @@ public class lembreteTest {
     LembreteService lembreteService;
 
     @MockBean
-    PessoaService pessoaService;
+    PessoaRepository pessoaRepository;
 
 
+@BeforeEach
+    void criarPessoa(){
+    Optional<Pessoa> pessoa = Optional.of(new Pessoa(1L, "Eduardo"));
+    Lembrete lembrete = new Lembrete(1L,"Lembrete 1",pessoaRepository.findById(1L).get());
+
+    Mockito.when(pessoaRepository.findById(1L)).thenReturn(pessoa);
+
+    Mockito.when(lembreteService.cadastrar(lembrete)).thenReturn(lembrete);
 
 
+}
 
+@Test
+    public void cadastrarLembreteTest(){
+
+    Assertions.assertEquals(lembreteService.cadastrar(new Lembrete(1L,"Lembrete 1",pessoaRepository.findById(1L).get())));
+
+
+}
 
 
 

42d69a65c8106780da38112034543d9aa95dae53
Luis Rodriguez <luiserr1394@gmail.com>
2023-09-06 23:14:24 -0300

    testes lembretes

--- src/test/java/com/desafio/lembrete/servicetest/LembreteTest.java
+++ src/test/java/com/desafio/lembrete/servicetest/LembreteTest.java
@@ -3,6 +3,7 @@ package com.desafio.lembrete.servicetest;
 
 import com.desafio.lembrete.entity.Lembrete;
 import com.desafio.lembrete.entity.Pessoa;
+import com.desafio.lembrete.repository.LembreteRepository;
 import com.desafio.lembrete.repository.PessoaRepository;
 import com.desafio.lembrete.service.LembreteService;
 import com.desafio.lembrete.service.PessoaService;
@@ -16,35 +17,53 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Optional;
 
 @SpringBootTest
-public class lembreteTest {
+public class LembreteTest {
 
     @MockBean
     LembreteService lembreteService;
+
+    @MockBean
+    LembreteRepository lembreteRepository;
 
     @MockBean
     PessoaRepository pessoaRepository;
 
 
 @BeforeEach
-    void criarPessoa(){
-    Optional<Pessoa> pessoa = Optional.of(new Pessoa(1L, "Eduardo"));
-    Lembrete lembrete = new Lembrete(1L,"Lembrete 1",pessoaRepository.findById(1L).get());
+    void criarLembrete(){
+    Pessoa pessoa = new Pessoa(1L, "Eduardo");
+    Lembrete lembrete = new Lembrete(1L,"Lembrete 1",pessoa);
 
-    Mockito.when(pessoaRepository.findById(1L)).thenReturn(pessoa);
+    Mockito.when(pessoaRepository.findById(1L)).thenReturn(Optional.of(pessoa));
+    Mockito.when(lembreteRepository.findById(1L)).thenReturn(Optional.of(lembrete));
 
     Mockito.when(lembreteService.cadastrar(lembrete)).thenReturn(lembrete);
 
 
 }
 
-/*@Test
+@Test
     public void cadastrarLembreteTest(){
 
-    Assertions.assertEquals(lembreteService.cadastrar(new Lembrete(1L,"Lembrete 1",pessoaRepository.findById(1L).get())));
+    var lembrete = lembreteRepository.findById(1L).get();
 
-
+    Assertions.assertEquals(lembrete,lembreteService.cadastrar(lembrete));
 }
-*/
+
+
+@Test
+    public void findLembreteByIdTest(){
+
+    Optional<Lembrete> lembreteTest = lembreteRepository.findById(1L);
+
+    Long id = lembreteTest.get().getId();
+
+    Assertions.assertEquals(lembreteTest,lembreteRepository.findById(id));
+}
+
+
+
+
 
 
 
